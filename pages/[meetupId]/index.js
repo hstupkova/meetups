03873877ae242a5a34e477ff31e4d0c23ce6afe4
Hana Stupková <hstupkova@gmail.com>
2021-11-24 16:45:21 +0100

    pages setup

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Brno_Parnas_Fountain-02.jpg/1280px-Brno_Parnas_Fountain-02.jpg"
+      title="The First Meetup"
+      address="Zelný trh 295, 602 00 Brno"
+      description="Our first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'The First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Brno_Parnas_Fountain-02.jpg/1280px-Brno_Parnas_Fountain-02.jpg',
+    address: 'Zelný trh 295, 602 00 Brno',
+    desctiption: 'Our first meetup',
+  },
+  {
+    id: 'm2',
+    title: 'The Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Brno_Parnas_Fountain-02.jpg/1280px-Brno_Parnas_Fountain-02.jpg',
+    address: 'Zelný trh 295, 602 00 Brno',
+    desctiption: 'Our second meetup',
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
